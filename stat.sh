962b219bd48c4dd092b567dfac616202b83da06e
afrolovskiy <frolovskiyav@gmail.com>
2016-10-11 14:56:11 +0300

    update stat.sh

--- stat.sh
+++ stat.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+LOADAVG_FILE=~/loadagv.10000ws.1000rps
+NETSTAT_FILE=~/netstat.10000ws.1000rps
 while true
 do
     # Log load average
-    echo "$(date '+TIME:%H:%M:%S') $(uptime)" | tee -a ~/loadagv
+    echo "$(date '+TIME:%H:%M:%S') $(uptime)" >> $LOADAVG_FILE
     # Log network stat
-    echo "$(date '+TIME:%H:%M:%S') $(netstat -i | grep eth0)" | tee -a ~/netstat
+    echo "$(date '+TIME:%H:%M:%S') $(netstat -i | grep eth0)" >> $NETSTAT_FILE
     sleep 1
 done
