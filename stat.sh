10c6f874d1bb8ac6f1339f964fddb3f405670604
afrolovskiy <frolovskiyav@gmail.com>
2016-10-12 14:50:16 +0300

    fix stat.sh uptime output

--- stat.sh
+++ stat.sh
@@ -4,7 +4,7 @@ NETSTAT_FILE=~/netstat.10000ws.1000rps
 while true
 do
     # Log load average
-    echo "$(date '+TIME:%H:%M:%S') $(uptime)" >> $LOADAVG_FILE
+    uptime >> $LOADAVG_FILE
     # Log network stat
     echo "$(date '+TIME:%H:%M:%S') $(netstat -i | grep eth0)" >> $NETSTAT_FILE
     sleep 1
