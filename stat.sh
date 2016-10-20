9052573251aa518d65f726252dab3c386723e762
afrolovskiy <frolovskiyav@gmail.com>
2016-10-20 13:42:33 +0300

    fixed stat.sh

--- stat.sh
+++ stat.sh
@@ -7,7 +7,7 @@ NICSTAT_FILE=~/nicstat.10000ws.1000rps
 while true
 do
     uptime >> $LOADAVG_FILE
-    echo "$(date '+TIME:%H:%M:%S') $(netstat -s | grep 'segments retransmited')" > $NETSTAT_S_FILE
+    echo "$(date '+TIME:%H:%M:%S') $(netstat -s | grep 'segments retransmited')" >> $NETSTAT_S_FILE
     echo "$(date '+TIME:%H:%M:%S') $(netstat -i | grep eth0)" >> $NETSTAT_I_FILE
     echo "$(date '+TIME:%H:%M:%S') $(nicstat)" >> $NICSTAT_FILE
     sleep $INTERVAL
