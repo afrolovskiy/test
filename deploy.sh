5107abe42e48e37bebbe2b96af9d2e80b4314b66
afrolovskiy <frolovskiyav@gmail.com>
2016-10-20 12:28:13 +0300

    added stat.sh copying to deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ GOOS=linux go build -o=test-server server/main.go
 
 scp -i $SSH_KEY test-client ubuntu@$HOST_CLIENT:/home/ubuntu/
 scp -i $SSH_KEY test-server ubuntu@$HOST_SERVER:/home/ubuntu/
+scp -i $SSH_KEY stat.sh ubuntu@$HOST_SERVER:/home/ubuntu/
  
