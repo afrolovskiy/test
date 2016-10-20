7b3d716682fc6c034c433b18d85744a6c549cefa
afrolovskiy <frolovskiyav@gmail.com>
2016-10-20 12:32:07 +0300

    added vmstat to stat.sh

--- stat.sh
+++ stat.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-LOADAVG_FILE=~/loadagv.10000ws.1000rps
+INTERVAL=1
+LOADAVG_FILE=~/loadavg.10000ws.1000rps
 NETSTAT_FILE=~/netstat.10000ws.1000rps
+VMSTAT_FILE=~/vmstat.10000ws.1000rps
 while true
 do
-    # Log load average
     uptime >> $LOADAVG_FILE
-    # Log network stat
+    echo "$(date '+TIME:%H:%M:%S') $(vmstat | tail -n 1)" >> $VMSTAT_FILE
     echo "$(date '+TIME:%H:%M:%S') $(netstat -i | grep eth0)" >> $NETSTAT_FILE
-    sleep 1
+    sleep $INTERVAL
 done
