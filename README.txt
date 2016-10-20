6690422ec3fa7c1f06c5e681d9956277a9d190d3
afrolovskiy <frolovskiyav@gmail.com>
2016-10-20 12:57:45 +0300

    extended README

--- README.txt
+++ README.txt
@@ -4,3 +4,24 @@ Examples:
 prlimit --nofile=20000: ./test-server
 1) cient run:
 prlimit --nofile=20000: ./test-client
+
+Monitoring:
+CPU monitoring
+vmstat 1
+
+Memory monitoring
+vmstat 1
+dmesg (call at the end of script work)
+
+Network interface monitoring
+sar -n DEV 1
+sar -n EDEV 1
+netstat -i
+netstat -s | grep 'segments retransmited'
+nicstat
+
+Run monitor test-server:
+source stat.sh
+vmstat 1 > ~/vmstat.10000ws.1000rps
+sar -n DEV 1 > ~/sar.dev.10000ws.1000rps
+sar -n EDEV 1 > ~/sar.dev.10000ws.1000rps
--- stat.sh
+++ stat.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 INTERVAL=1
 LOADAVG_FILE=~/loadavg.10000ws.1000rps
-NETSTAT_FILE=~/netstat.10000ws.1000rps
-VMSTAT_FILE=~/vmstat.10000ws.1000rps
+NETSTAT_S_FILE=~/netstat.s.10000ws.1000rps
+NETSTAT_I_FILE=~/netstat.i.10000ws.1000rps
+NICSTAT_FILE=~/nicstat.10000ws.1000rps
 while true
 do
     uptime >> $LOADAVG_FILE
-    echo "$(date '+TIME:%H:%M:%S') $(vmstat | tail -n 1)" >> $VMSTAT_FILE
-    echo "$(date '+TIME:%H:%M:%S') $(netstat -i | grep eth0)" >> $NETSTAT_FILE
+    echo "$(date '+TIME:%H:%M:%S') $(netstat -s | grep 'segments retransmited')" > $NETSTAT_S_FILE
+    echo "$(date '+TIME:%H:%M:%S') $(netstat -i | grep eth0)" >> $NETSTAT_I_FILE
+    echo "$(date '+TIME:%H:%M:%S') $(nicstat)" >> $NICSTAT_FILE
     sleep $INTERVAL
 done
